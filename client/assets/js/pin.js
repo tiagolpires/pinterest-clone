3bd1dc0bbcacc50204cad65da7380afc9674b1e3
unknown <tiagolpires@hotmail.com>
2021-04-21 20:17:45 -0300

    Save Pin on Client

--- client/assets/js/pin.js
+++ client/assets/js/pin.js
@@ -1,27 +1,33 @@
 const pinId = new URLSearchParams(location.search).get('id')
 const apiBaseUrl = 'http://localhost:3001/graphql'
+const googleLoginPage = 'http://localhost:3001/auth/google'
 
 createPin()
 
 async function createPin() {
-  const pin = await getPin()
+  const query = `{ pin(id: ${pinId}){image_url, title, id, author{name, profile_picture}}}`
+  const url = `${apiBaseUrl}/${pinId}`
+  const pin = await fetchApi(query, url, 'pin')
   addPin(pin)
 }
 
-async function getPin() {
-    const query = `{ pin(id: ${pinId}){image_url, title, id, author{name, profile_picture}}}`
-
-    const response = await fetch(`${apiBaseUrl}/${pinId}`, {
+async function fetchApi(query, url, dataName) {
+  const response = await fetch(url, {
     method: 'POST',
+    credentials: "include",
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
+      "Access-Control-Allow-Credentials": true
     },
-    body: JSON.stringify( { query } )
+    body: JSON.stringify({ 
+      query: query  
+    })
   })
-
-    const pin = await response.json()
-    return pin.data.pin
+    
+  const data = await response.json()
+  if(data.errors) return window.location.href = googleLoginPage
+  return data.data[dataName]
 }
 
 function addPin(pin) {
@@ -35,7 +41,7 @@ function addPin(pin) {
               <img src=${pin.author.profile_picture}>
               <span>Pin de <b>${pin.author.name}</b></span>
           </div>
-          <a>Salvar</a>
+          <a class="pin-save-btn" onClick = savePin(${pin.id})>Salvar</a>
       </div>
       <div class="pin-title">
           <h1>${pin.title}</h1>
@@ -43,4 +49,16 @@ function addPin(pin) {
     </div>
   `
   pinContainer.innerHTML = pinContent
+}
+
+async function savePin(pinId) {
+  const query = `mutation {savePin(id: "${pinId}"){user_id pin_id}}`
+  const res = await fetchApi(query, apiBaseUrl, 'savePin')
+  changeSaveBtn()
+}
+
+function changeSaveBtn() {
+  const saveBtn = document.querySelector('.pin-save-btn')
+  saveBtn.classList.add('saved')
+  saveBtn.innerText = 'Salvo'
 }
